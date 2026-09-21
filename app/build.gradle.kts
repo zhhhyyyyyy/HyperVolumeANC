272a2e8d2969bb0f6305674930cd9eee06a1bcb2
zhhhyyyyyy <zhhhyyyyyy@users.noreply.github.com>
2026-09-21 18:25:33 +0800

    release: HyperVolumeANC 1.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "io.github.zhhhyyyyyy.hypervolumeanc"
         minSdk = 34
         targetSdk = 36
-        versionCode = 32
-        versionName = "1.5.12"
+        versionCode = 33
+        versionName = "1.6"
     }
 
     buildTypes {
